da57b135a8373843e6afca6c3ca106fc86fa9df0
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-02-15 17:53:01 +0100

    more 'get elements' and 'get data' in exampleView

--- dinnerplanner-html-master/js/view/exampleView.js
+++ dinnerplanner-html-master/js/view/exampleView.js
@@ -6,23 +6,64 @@ var ExampleView = function (container, model) {
 
     //*** we need to make all of these ids in the html files!
 
-    //home screen
+    //GET ELEMENTS
+
+    //home screen (View 1)
     this.createNewDinnerButton = container.find("#createNewDinnerButton");
 
-    //my dinner sidebar
+    //my dinner sidebar (View 2)
     this.numberOfGuests = container.find("#numberOfGuests");
     this.plusButton = container.find("#plusGuest");
     this.minusButton = container.find("#minusGuest");
-    this.selectedDish = container.find("selectedDish");
-    this.selectedDishCost = container.find("selectedDishCost");
+    this.fullMenuList = container.find("#fullMenuList");
     this.totalDinnerCost = container.find("#totalDinnerCost");
     this.confirmDinnerButton = container.find("#confirmDinnerButton");
+    this.removeDishButton = container.find("#removeDishButton"); //this function exists in dinnerModel, so I'm guessing it will be used in View 2?
+
+    //select dish (View 3)
+    this.searchBar = container.find("#searchBar");
+    this.searchButton = container.find("#searchButton");
+    this.dishTypeSelector = container.find("#dishTypeSelector");
+    this.allDishes = container.find("#allDishes");
+
+    //dish details (View 4)
+    this.backToSelectDishButton = container.find("#backToSelectDishButton");
+    this.confirmDishButton = container.find("#confirmDishButton");
+    this.dishDetails = container.find("#dishDetails");
+
+    //dinner overview (View 5)
+    this.printRecipeButton = container.find("#printRecipeButton");
+    this.overviewBackButton = container.find("#overviewBackButton");
+    this.overviewFullMenuList = container.find("#overviewFullMenuList");
+    this.overviewTotalDinnerCost = container.find("#overviewTotalDinnerCost");
+
+    //instructions screen (View 6)
+    this.instructionsBackButton = container.find("#instructionsBackButton");
+    this.instructionsFullMenuList = container.find("#instructionsFullMenuList");
 
 
-    //select dish screen
 
+    //GET DATA
 
-
+    //my dinner sidebar (View 2)
     this.numberOfGuests.html(model.getNumberOfGuests());
+    this.fullMenuList.html(model.getFullMenu());
+    this.totalDinnerCost.html(model.getTotalMenuPrice());
+    this.removeDishButton.html(model.removeDishFromMenu());
+
+    //select dish (View 3)
+    this.allDishes.html(model.getAllDishes()); //takes type and filter parameters (get them from dishTypeSelector)
+
+    //dish details (View 4)
+    this.dishDetails.html(model.getDish()); //takes id parameter
+    this.confirmDishButton.html(model.addDishToMenu()); //takes id parameter
+
+    //dinner overview (View 5)
+    this.overviewFullMenuList.html(model.getFullMenu());
+    this.overviewTotalDinnerCost.html(model.getTotalMenuPrice());
+
+    //instructions screen (View 6)
+    this.overviewFullMenuList.html(model.getFullMenu());
+
 
 }
