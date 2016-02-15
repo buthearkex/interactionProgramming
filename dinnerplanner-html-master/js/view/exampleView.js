7c19149a17e2ecf8ce4cb0430d16f10f284036af
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-02-15 11:00:35 +0100

    started adding get data code to exampleView

--- dinnerplanner-html-master/js/view/exampleView.js
+++ dinnerplanner-html-master/js/view/exampleView.js
@@ -3,9 +3,24 @@ var ExampleView = function (container) {
 
     // Get all the relevant elements of the view (ones that show data
     // and/or ones that responed to interaction)
+	
+	//*** we need to make all of these ids in the html files!
+
+	//home screen
+    this.createNewDinnerButton = container.find("#createNewDinnerButton");
+
+	//my dinner sidebar
     this.numberOfGuests = container.find("#numberOfGuests");
     this.plusButton = container.find("#plusGuest");
     this.minusButton = container.find("#minusGuest");
+    this.selectedDish = container.find("selectedDish");
+    this.selectedDishCost = container.find("selectedDishCost");
+    this.totalDinnerCost = container.find("#totalDinnerCost");
+    this.confirmDinnerButton = container.find("#confirmDinnerButton");
+
+    //select dish screen
+
+    
 
     this.numberOfGuests.html(model.getNumberOfGuests());
 
