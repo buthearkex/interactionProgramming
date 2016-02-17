122290a594b683760bbef44974b18118e64ee955
buthearkex <mikko.s.honkanen@aalto.fi>
2016-02-17 14:52:36 +0100

    procedural

--- dinnerplanner-html-master/js/view/exampleView.js
+++ dinnerplanner-html-master/js/view/exampleView.js
@@ -176,4 +176,25 @@ var ExampleView = function (container, model) {
 
     $('#totalDinnerCost').html(model.getTotalMenuPrice());
 
+    $('.nbrGuests').html(model.getNumberOfGuests());
+
+    var ingreds = menuItems[0].ingredients;
+    for (var i = 0; i < ingreds.length; i++) {
+        $('#ingredientList').append(
+            "<div class='row'>" +
+            "<div class='col-sm-3'>" +
+            ingreds[i].name +
+            "</div>" +
+            "<div class='col-sm-3'>" +
+            ingreds[i].quantity +
+            "</div>" +
+            "<div class='col-sm-3'>SEK</div>" +
+            "<div class='col-sm-3'>" +
+            ingreds[i].price +
+            "</div>" +
+            "</div>"
+        );
+    }
+
+
 }
