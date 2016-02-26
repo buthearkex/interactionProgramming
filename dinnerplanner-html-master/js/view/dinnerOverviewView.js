b175e69418ef4ac93d9dc7a5042ff020decf3940
buthearkex <mikko.s.honkanen@aalto.fi>
2016-02-26 13:49:41 +0100

    overview

--- dinnerplanner-html-master/js/view/dinnerOverviewView.js
+++ dinnerplanner-html-master/js/view/dinnerOverviewView.js
@@ -1,33 +1,36 @@
 var DinnerOverviewView = function (container, model) {
 
+    model.addObserver(this);
+
     this.overviewFullMenuList = container.find('#overviewFullMenuList');
     this.printButton = container.find('#print');
 
-    var overviewItems = model.getFullMenu();
-    for (var i = 0; i < overviewItems.length; i++) {
+    this.updateMenu = function () {
+        var overviewItems = model.getFullMenu();
+        for (var i = 0; i < overviewItems.length; i++) {
+            this.overviewFullMenuList.append(
+                "<div class='col-md-3 thumbnail'>" +
+                "<img class='img-responsive overview-pic' src='images/" +
+                overviewItems[i].image +
+                "'>" +
+                "<p>" +
+                model.getPriceOfDish(overviewItems[i].id) +
+                " SEK</p>" +
+                "</div>"
+            );
+        }
+
         this.overviewFullMenuList.append(
-            "<div class='col-md-3 thumbnail'>" +
-            "<img class='img-responsive overview-pic' src='images/" +
-            overviewItems[i].image +
-            "'>" +
+            "<div class='col-md-3 border'>" +
+            "<p>Total:</p>" +
+            "<div>" +
             "<p>" +
-            model.getPriceOfDish(overviewItems[i].id) +
+            model.getTotalMenuPrice() +
             " SEK</p>" +
+            "</div>" +
             "</div>"
         );
     }
-
-    this.overviewFullMenuList.append(
-        "<div class='col-md-3 border'>" +
-        "<p>Total:</p>" +
-        "<div>" +
-        "<p>" +
-        model.getTotalMenuPrice() +
-        " SEK</p>" +
-        "</div>" +
-        "</div>"
-    );
-
 
     this.hide = function () {
         container.hide();
@@ -37,5 +40,12 @@ var DinnerOverviewView = function (container, model) {
         container.show();
     }
 
+    this.update = function (obj) {
+        if (obj == 'newDish') {
+            this.updateMenu();
+        }
+    }
+
+    this.updateMenu();
 
 }
