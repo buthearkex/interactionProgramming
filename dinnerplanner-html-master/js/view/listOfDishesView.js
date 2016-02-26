a82c82de1529767874d8144703dbd7fbf89d340a
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-02-26 16:35:37 +0100

    filter by dish type

--- dinnerplanner-html-master/js/view/listOfDishesView.js
+++ dinnerplanner-html-master/js/view/listOfDishesView.js
@@ -2,25 +2,29 @@ var ListOfDishesView = function (container, model) {
 
     this.allDishes = container.find('#allDishes');
 
-    var dishes = model.getAllDishes().prevObject;
-    for (var i = 0; i < dishes.length; i++) {
-        this.allDishes.append(
-            "<div class='col-md-3'>" +
-            "<a id='" +
-            dishes[i].id +
-            "' class='food-link'>" +
-            "<img class='img-responsive food-pic' src='images/" +
-            dishes[i].image +
-            "'>" +
-            "<h2>" +
-            dishes[i].name +
-            "</h2>" +
-            "</a>" +
-            "<p>" +
-            dishes[i].description +
-            "</p>" +
-            "</div>"
-        );
+    this.updateAllDishes = function (type) {
+        var dishes = model.getAllDishes().prevObject;
+        for (var i = 0; i < dishes.length; i++) {
+            if (dishes[i].type == type || type == All) {
+                this.allDishes.append(
+                    "<div class='col-md-3'>" +
+                    "<a id='" +
+                    dishes[i].id +
+                    "' class='food-link'>" +
+                    "<img class='img-responsive food-pic' src='images/" +
+                    dishes[i].image +
+                    "'>" +
+                    "<h2>" +
+                    dishes[i].name +
+                    "</h2>" +
+                    "</a>" +
+                    "<p>" +
+                    dishes[i].description +
+                    "</p>" +
+                    "</div>"
+                );
+            }
+        }
     }
 
     this.foodLinks = container.find('.food-link');
@@ -31,4 +35,21 @@ var ListOfDishesView = function (container, model) {
     this.show = function () {
         container.show();
     }
+
+    
+    this.update = function (type) {
+        if (type == "Starter") {
+            this.updateAllDishes(Starter);
+        } else if (type == "Main Dish") {
+            this.updateAllDishes(Main);
+        } else if (type == "Dessert") {
+            this.updateAllDishes(Dessert);
+        } else {
+            this.updateAllDishes(All);
+        }
+        
+    }
+
+    this.updateAllDishes(All);
+    
 }
