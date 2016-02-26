0ee73f0b9bc2b2ef069c5165366d29f8483ee656
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-02-26 14:04:09 +0100

    update recipe view

--- dinnerplanner-html-master/js/view/recipeView.js
+++ dinnerplanner-html-master/js/view/recipeView.js
@@ -1,30 +1,34 @@
 var RecipeView = function (container, model) {
 
+    model.addObserver(this);
+
     this.recipe = container.find('#recipe-container');
 
-    var menuItems = model.getFullMenu();
-    for (var i = 0; i < menuItems.length; i++) {
-        this.recipe.append(
-            "<div class='row'>" +
-            "<div class='col-md-5'>" +
-            "<img class='food-pic' src='images/" +
-            menuItems[i].image +
-            "'>" +
-            "<h3>" +
-            menuItems[i].name +
-            "</h3>" +
-            "<p>" +
-            menuItems[i].description +
-            "</p>" +
-            "</div>" +
-            "<div class='col-md-7'>" +
-            "<h4>PREPARATION</h4>" +
-            "<p>" +
-            menuItems[i].description +
-            "</p>" +
-            "</div>" +
-            "</div>"
-        );
+    this.updateRecipe = function () {
+        var menuItems = model.getFullMenu();
+        for (var i = 0; i < menuItems.length; i++) {
+            this.recipe.append(
+                "<div class='row'>" +
+                "<div class='col-md-5'>" +
+                "<img class='food-pic' src='images/" +
+                menuItems[i].image +
+                "'>" +
+                "<h3>" +
+                menuItems[i].name +
+                "</h3>" +
+                "<p>" +
+                menuItems[i].description +
+                "</p>" +
+                "</div>" +
+                "<div class='col-md-7'>" +
+                "<h4>PREPARATION</h4>" +
+                "<p>" +
+                menuItems[i].description +
+                "</p>" +
+                "</div>" +
+                "</div>"
+            );
+        }
     }
 
     this.hide = function () {
@@ -35,4 +39,11 @@ var RecipeView = function (container, model) {
         container.show();
     }
 
+    this.update = function (obj) {
+        if (obj == 'newDish') {
+            this.updateRecipe();
+        }
+    }
+
+    this.updateRecipe();
 }
