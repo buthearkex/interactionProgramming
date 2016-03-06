4117296b482579b6ce4b44a3b3a8bfde047096fe
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-03-06 22:45:33 +0100

    Added some html and getTotalMenuPrice

--- lab5/app/js/dinnerService.js
+++ lab5/app/js/dinnerService.js
@@ -3,34 +3,42 @@
 // dependency on any service you need. Angular will insure that the
 // service is created first time it is needed and then just reuse it
 // the next time.
-dinnerPlannerApp.factory('Dinner',function ($resource) {
-  
-  var numberOfGuest = 2;
+dinnerPlannerApp.factory('Dinner', function ($resource) {
+
+    var numberOfGuest = 2;
+    var totalPrice = 100;
 
 
-  this.setNumberOfGuests = function(num) {
-    numberOfGuest = num;
-  }
+    this.setNumberOfGuests = function (num) {
+        numberOfGuest = num;
+    }
 
-  this.getNumberOfGuests = function() {
-    return numberOfGuest;
-  }
+    this.getNumberOfGuests = function () {
+        return numberOfGuest;
+    }
 
-
-  // TODO in Lab 5: Add your model code from previous labs
-  // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
-  // a bit to take the advantage of Angular resource service
-  // check lab 5 instructions for details
+    this.getTotalMenuPrice = function () {
+        return totalPrice;
+    }
 
 
 
 
 
-  // Angular service needs to return an object that has all the
-  // methods created in it. You can consider that this is instead
-  // of calling var model = new DinnerModel() we did in the previous labs
-  // This is because Angular takes care of creating it when needed.
-  return this;
+    // TODO in Lab 5: Add your model code from previous labs
+    // feel free to remove above example code
+    // you will need to modify the model (getDish and getAllDishes) 
+    // a bit to take the advantage of Angular resource service
+    // check lab 5 instructions for details
+
+
+
+
+
+    // Angular service needs to return an object that has all the
+    // methods created in it. You can consider that this is instead
+    // of calling var model = new DinnerModel() we did in the previous labs
+    // This is because Angular takes care of creating it when needed.
+    return this;
 
 });
